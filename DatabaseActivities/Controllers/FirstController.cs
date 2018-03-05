3cf47e4444ad9e5035aced258e3c32f67c32737e
Unknown <19syverj@elmbrookstudents.org>
2018-03-04 22:29:38 -0600

    added even more stuff

--- DatabaseActivities/Controllers/FirstController.cs
+++ DatabaseActivities/Controllers/FirstController.cs
@@ -14,6 +14,28 @@ namespace DatabaseActivities.Controllers
             return View();
         }
 
-
+        public ActionResult HorsesToBlocks()
+        {
+            return View("HorsesToBlocks", model: "");
+        }
+        [HttpPost]
+        public ActionResult ConvertHorses(string type, double number)
+        {
+            if (type.Equals("horses") || type.Equals("Horses"))
+            {
+                return View("HorsesToBlocks", model: "Horses = " + number * 33.3333333333);
+            }
+            else if (type.Equals("blocks") || type.Equals("Blocks"))
+            {
+                return View("HorsesToBlocks", model: "Blocks = " + number * 0.03);
+            }
+            else
+            {
+                return View("UnitConversion", model: "ERROR");
+            }
+        }
     }
+
+
+
 }

90df587771d293010a14d29d92b291dff09b7462
su20 <19liuq@elmbrookstudents.org>
2018-03-04 20:22:42 -0600

    base conversion

--- DatabaseActivities/Controllers/FirstController.cs
+++ DatabaseActivities/Controllers/FirstController.cs
@@ -13,7 +13,14 @@ namespace DatabaseActivities.Controllers
         {
             return View();
         }
-
-
+        public ActionResult BaseConversion()
+        {
+            return View(viewName: "BaseConversion", model: " ");
+        }
+        [HttpPost]
+        public ActionResult BaseConversion(int startNumber)
+        {
+            return View("BaseConversion", model: "Your number will be: " + Convert.ToString(startNumber, 2));
+        }
     }
 }

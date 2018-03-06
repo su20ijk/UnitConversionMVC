2389e2ad48fb8d4d298f02a9432e80cee6f2c615
Jacob Patrick <fivestaroblivion@gmail.com>
2018-03-06 06:39:44 -0600

    Added my work

---
+++ DatabaseActivities/Controllers/UnitConversionController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DatabaseActivities.Controllers
+{
+    public class UnitConversionController : Controller
+    {
+        // GET: UnitConversion
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GramToOunce()
+        {
+            return View("GramToOunce", "");
+        }
+
+        [HttpPost]
+        public ActionResult GramToOunce(double Grams)
+        {
+            return View(viewName: "GramToOunce", model: "Ounces: " + Grams * 0.035274);
+        }
+    }
+}
